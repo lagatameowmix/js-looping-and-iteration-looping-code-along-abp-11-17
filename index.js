a323652bf0f571cb58570cf7a4769ee5734742e1
Sasha V <sashav4@gmail.com>
2017-11-13 05:08:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solutions in this file
-const gifts = ['teddy bear', 'drone', 'doll'];
- 
-function wrapGifts (gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+const names = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges() {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #[i-1].`);
   }
- 
-  return gifts;
+
+  return names;
 }
- 
-wrapGifts(gifts);
+
+printBadges(names);
