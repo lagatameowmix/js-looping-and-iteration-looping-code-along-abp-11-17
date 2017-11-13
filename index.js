734d3db0258c4d36bd8c051ca74ece67e33e4500
Sasha V <sashav4@gmail.com>
2017-11-13 05:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const names = ['Ada', 'Brendan', 'Ali'];
 
-function printBadges() {
+function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
     console.log(`Welcome ${names[i]}! You are employee #[i-1].`);
   }
